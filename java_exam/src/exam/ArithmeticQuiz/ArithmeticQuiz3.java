0744aedcb4d8f4a3608b4e1cae216e25dfa99b7c
KimTae-wann <xodhks9711@naver.com>
2026-01-19 10:21:35 +0900

    0119 first

--- java_exam/src/exam/ArithmeticQuiz/ArithmeticQuiz3.java
+++ java_exam/src/exam/ArithmeticQuiz/ArithmeticQuiz3.java
@@ -1,4 +1,4 @@
-package exam;
+package exam.ArithmeticQuiz;
 
 public class ArithmeticQuiz3 {
 
---
+++ java_exam/src/exam/FlowControlQuiz/FlowControlQuiz1.java
@@ -0,0 +1,34 @@
+package exam.FlowControlQuiz;
+
+public class FlowControlQuiz1 {
+	
+	public static void main(String[] args) {
+		int korScore = 90;
+		int engScore = 88;
+		int mathScore = 70;
+		int progScore = 80;
+		
+		int sum = korScore + engScore + mathScore + progScore;
+		double average = sum / 4d;
+		
+		if (average >= 95) {
+			System.out.println("A+");
+		}
+		else if (average >= 90) {
+			System.out.println("A");
+		}
+		else if (average >= 85) {
+			System.out.println("B+");
+		}
+		else if (average >= 80) {
+			System.out.println("B");
+		}
+		else if (average >= 70) {
+			System.out.println("C");
+		}
+		else {
+			System.out.println("F");
+		}
+	}
+
+}
