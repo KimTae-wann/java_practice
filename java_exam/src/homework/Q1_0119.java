83df0ae54e166094ddbae53bd8a736d86ecc812c
KimTae-wann <xodhks9711@naver.com>
2026-01-19 21:43:53 +0900

    0119 hm

--- java_exam/src/homework/Q1_0119.java
+++ java_exam/src/homework/Q1_0119.java
@@ -4,6 +4,21 @@ import java.util.Scanner;
 
 public class Q1_0119 {
 	
+	public static void main(String[] args) {
+		for (int i = 2; i <= 100_000; i++) { // 0과 1은 소수가 아니므로, 2부터 100,000까지 소수 판별
+			boolean isPrime = true; // 소수인지 판별하는 check 변수
+			for (int j = 2; j < i; j++) { // 2부터 본인의 직전 수까지
+				if (i % j == 0) { // 나눠본다
+					isPrime = false; // 나누어 떨어지면 소수 아님
+				}
+			}
+			if (isPrime == true) { // 소수면
+				System.out.println(i + " "); // 출력
+			}
+		}
+	}
+	
+	/*
 	static boolean[] notPrimeNumber = new boolean[100001];
 	
 	public static void main(String[] args) {
@@ -29,4 +44,5 @@ public class Q1_0119 {
 			}
 		}
 	}
+	*/
 }
