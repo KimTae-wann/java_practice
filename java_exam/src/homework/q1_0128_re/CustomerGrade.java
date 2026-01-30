23b29433c913b7d26cf9101bbf31b7207a3da416
KimTae-wann <xodhks9711@naver.com>
2026-01-30 13:02:39 +0900

    0128 hm

--- java_exam/src/homework/q1_0128_re/CustomerGrade.java
+++
@@ -1,5 +0,0 @@
-package homework.q1_0128_re;
-
-public enum CustomerGrade {
-	Normal, VIP, VVIP;
-}
--- java_exam/src/homework/q1_0128_re/Store.java
+++
@@ -1,6 +0,0 @@
-package homework.q1_0128_re;
-
-public interface Store {
-	
-	void sell (Customer customer, Product product, int moneyPaid, int pointPaid);
-}
